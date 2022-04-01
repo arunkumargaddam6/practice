57510a4efb463b253fedca5b7e93207c316a891d
arunkumargaddam6 <95743741+arunkumargaddam6@users.noreply.github.com>
2022-04-01 12:25:05 +1100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+region = "ap-southeast-2"
+}
+
+resource "aws_instance" "testinstance" {
+  ami           = "ami-059af0b76ba105e7e"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "arun"
+  }
+}
